8d19bc404e2405098bf42603cc8374b9e6e50e1a
fan <xiaofanms@outlook.com>
2021-09-15 11:07:21 +0800

    由于artifact有有效期, 所以修改为一星期更新一次。

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,6 @@ import * as extract from "extract-zip";
 import { startTasks } from "./tasks";
 
 /**
- * 获取最新的checksum
- * @param $ cheerio
- * @returns checksum
- */
- async function getRemoteChecksum($: cheerio.Root) {
-  const href = $('a[href$="checksum"]').attr('href');
-  const rsp = await axios.get(href);
-  return rsp.data;
-}
-
-/**
  * 下载最新的sde包
  * @param $ cheerio
  */
@@ -34,33 +23,17 @@ async function downloadSDE($: cheerio.Root) {
   fs.unlinkSync('tmp.zip');
 }
 
-/**
- * 更新本地的checksum
- * @param checksum checksum
- */
- function updateChecksum(checksum: string) {
-  fs.writeFileSync('./checksum', checksum);
-}
-
 async function main() {
   if (fs.existsSync(__dirname + '/sde')) {
     console.log('清理sde目录');
     fs.rmdirSync(__dirname + '/sde', { recursive: true });
   }
-  const checksum = fs.readFileSync('./checksum').toString();
   const html = (await axios.get('https://developers.eveonline.com/resource/resources')).data;
   const $ = cheerio.load(html);
-  const remoteChecksum = await getRemoteChecksum($);
-  console.log(`checksum local:${checksum} -> remote:${remoteChecksum}`);
-  if (remoteChecksum === checksum) {
-    console.log('sde已是最新版, 跳过更新. checksum:' + checksum);
-    return;
-  }
   console.log('更新中');
   await downloadSDE($);
   // 开始任务
   await startTasks(__dirname + '/sde');
-  updateChecksum(remoteChecksum);
 }
 
 main().then(() => {
